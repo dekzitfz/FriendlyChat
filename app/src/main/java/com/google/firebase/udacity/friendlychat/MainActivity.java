a6b7b0ee50036d0a56ece2df6a318ef09ce35286
Adi Andrea <dekz.itfz@gmail.com>
2016-11-22 16:41:37 +0800

    impl send button to send data to RD firebase

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -115,6 +115,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // TODO: Send messages on click
 
+                FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(),mUsername,null);
+                //databaseReference.child("messages").push().setValue(friendlyMessage);
+                databaseReference.push().setValue(friendlyMessage);
+
                 // Clear input box
                 mMessageEditText.setText("");
             }
